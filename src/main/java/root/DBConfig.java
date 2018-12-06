d24bac76e4fb1b0e0ad1c4fc290e9b7864f01ca4
Barrya42 <Barrya42@gmail.com>
2018-12-07 00:28:54 +0700

    MessageHandler

--- src/main/java/root/DBConfig.java
+++ src/main/java/root/DBConfig.java
@@ -3,6 +3,7 @@ package root;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+import org.hibernate.cfg.Environment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -43,8 +44,8 @@ public class DBConfig
         Properties jpaProperties = new Properties();
         jpaProperties.put(org.hibernate.cfg.Environment.DIALECT, "org.hibernate.dialect.PostgreSQL9Dialect");
         jpaProperties.put(org.hibernate.cfg.Environment.HBM2DDL_AUTO, "update");
+        jpaProperties.put(Environment.NON_CONTEXTUAL_LOB_CREATION, true);
         //spring.jpa.properties.hibernate.temp.use_jdbc_metadata_defaults=false
-//        jpaProperties.put(org.hibernate.jpa.boot)
         entityManagerFactoryBean.setJpaProperties(jpaProperties);
 
         return entityManagerFactoryBean;
