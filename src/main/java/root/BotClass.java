4d60cd8a8f2ff1e1ae72b6cd1bfff8ce4be1acd4
Barrya42 <Barrya42@gmail.com>
2018-12-09 19:30:01 +0700

    MessageHandler

--- src/main/java/root/BotClass.java
+++ src/main/java/root/BotClass.java
@@ -11,7 +11,6 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import root.DBservices.RoleService;
 import root.DBservices.UserService;
-import root.entitys.UserEntity;
 
 @Service
 public class BotClass extends TelegramLongPollingBot implements InitializingBean
@@ -63,9 +62,9 @@ public class BotClass extends TelegramLongPollingBot implements InitializingBean
     @Override
     public void afterPropertiesSet() throws Exception
     {
-        UserEntity newUser = new UserEntity();
-        newUser.setId(455776162);
-        newUser.setUserRole(roleService.getOperatorRole());
-        userService.addUser(newUser);
+//        UserEntity newUser = new UserEntity();
+//        newUser.setId(455776162);
+//        newUser.setUserRole(roleService.getOperatorRole());
+//        userService.addUser(newUser);
     }
 }
